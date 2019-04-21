6bf88e03b4bfc4b70910fc4f7a814b3c53bc69d8
GanYihuan <583520052@qq.com>
2019-04-22 01:09:28 +0800

    3-3

--- code/array/lesson1.js
+++ code/array/lesson1.js
@@ -1,18 +1,23 @@
+/*
+ * @Description: 电话号码组合
+ * @version: 0.1
+ * @Author: GanEhank
+ * @LastEditors: GanEhank
+ * @Date: 2019-03-24 14:09:34
+ * @LastEditTime: 2019-04-22 00:56:11
+ */
+
 export default (str) => {
-  // 建立电话号码键盘映射
-  let map = ['', 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-  // 把输入字符串按单字符分隔变成数组，234=>[2,3,4]
-  let num = str.split('')
-  // 保存键盘映射后的字母内容，如 23=>['abc','def']
-  let code = []
+  let map = ['', 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'] // 建立电话号码键盘映射
+  let num = str.split('') // 把输入字符串按单字符分隔变成数组，234=>[2,3,4]
+  let code = [] // 保存键盘映射后的字母内容，如 23=>['abc','def']
   num.forEach(item => {
     if (map[item]) {
       code.push(map[item])
     }
   })
   let comb = (arr) => {
-    // 临时变量用来保存前两个组合的结果
-    let tmp = []
+    let tmp = [] // 临时变量用来保存前两个组合的结果
     // 最外层的循环是遍历第一个元素，里层的循环是遍历第二个元素
     for (let i = 0, il = arr[0].length; i < il; i++) {
       for (let j = 0, jl = arr[1].length; j < jl; j++) {
