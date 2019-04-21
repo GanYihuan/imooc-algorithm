4bd2e2d2b729bb9e6db65b683b21fd694aedd498
GanYihuan <583520052@qq.com>
2019-04-21 22:44:54 +0800

    init project

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,12 +1,10 @@
-// export default (str) => {
-//   // 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-//   let arr = str.split(' ')
-//   // 对数组进行遍历，然后每个元素进行反转
-//   let result = arr.map(item => {
-//     return item.split('').reverse().join('')
-//   })
-//   return result.join(' ')
-// }
+export default (str) => {
+  let arr = str.split(' ') // 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+  let result = arr.map(item => { // 对数组进行遍历，然后每个元素进行反转
+    return item.split('').reverse().join('')
+  })
+  return result.join(' ')
+}
 
 // export default (str) => {
 //   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
@@ -24,10 +22,10 @@
 //   }).join(' ')
 // }
 
-export default (str) => {
-  // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-  // 2. 对数组进行遍历，然后每个元素进行反转
-  return str.match(/[\w']+/g).map(item => {
-    return item.split('').reverse().join('')
-  }).join(' ')
-}
+// export default (str) => {
+//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+//   // 2. 对数组进行遍历，然后每个元素进行反转
+//   return str.match(/[\w']+/g).map(item => {
+//     return item.split('').reverse().join('')
+//   }).join(' ')
+// }
