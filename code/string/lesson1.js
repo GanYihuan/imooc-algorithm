e8223937ed5287a01c9c8fdc7c7e9fe613c3e286
GanYihuan <583520052@qq.com>
2019-04-21 23:33:11 +0800

    2-3

--- code/string/lesson1.js
+++ code/string/lesson1.js
@@ -1,31 +1,40 @@
+// export default (str) => {
+//   let arr = str.split(' ') // 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+//   let result = arr.map(item => { // 对数组进行遍历，然后每个元素进行反转
+//     return item.split('').reverse().join('')
+//   })
+//   return result.join(' ')
+// }
+
+// export default (str) => {
+//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+//   // 2. 对数组进行遍历，然后每个元素进行反转
+//   return str
+//     .split(' ')
+//     .map(item => {
+//       return item.split('').reverse().join('')
+//     })
+//     .join(' ')
+// }
+
+// export default (str) => {
+//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+//   // 2. 对数组进行遍历，然后每个元素进行反转
+//   return str
+//     .split(/\s/g)
+//     .map(item => {
+//       return item.split('').reverse().join('')
+//     })
+//     .join(' ')
+// }
+
 export default (str) => {
-  let arr = str.split(' ') // 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-  let result = arr.map(item => { // 对数组进行遍历，然后每个元素进行反转
-    return item.split('').reverse().join('')
-  })
-  return result.join(' ')
+  // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
+  // 2. 对数组进行遍历，然后每个元素进行反转
+  return str
+    .match(/[\w']+/g)
+    .map(item => {
+      return item.split('').reverse().join('')
+    })
+    .join(' ')
 }
-
-// export default (str) => {
-//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-//   // 2. 对数组进行遍历，然后每个元素进行反转
-//   return str.split(' ').map(item => {
-//     return item.split('').reverse().join('')
-//   }).join(' ')
-// }
-
-// export default (str) => {
-//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-//   // 2. 对数组进行遍历，然后每个元素进行反转
-//   return str.split(/\s/g).map(item => {
-//     return item.split('').reverse().join('')
-//   }).join(' ')
-// }
-
-// export default (str) => {
-//   // 1. 字符串按空格进行分隔，保存数组，数组的元素的先后顺序就是单词的顺序
-//   // 2. 对数组进行遍历，然后每个元素进行反转
-//   return str.match(/[\w']+/g).map(item => {
-//     return item.split('').reverse().join('')
-//   }).join(' ')
-// }
